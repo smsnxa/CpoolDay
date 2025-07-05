30387a810f77a70018a40b914966cd6389787312
smsnxa <sanaa.sarah@yahoo.com>
2025-07-05 12:43:49 +0000

    pas d'inspi

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -1,5 +1,13 @@
-// int main (void)
-// {
-//     my_print_n_ascii(5);
-//     return 
-// }
+void print_char(char c);
+
+void my_print_n_ascii(int howMany) {
+    for (int i = 110; i < 128; ++i){
+        print_char (i);
+    }
+    return; 
+}
+int main (void)
+{
+    my_print_n_ascii(5);
+    return 0;
+}
